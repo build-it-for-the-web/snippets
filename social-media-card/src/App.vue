b137c4300f1235f00cb580850ba8bffbd1bf82f3
mSutisna <manno@producthero.com>
2025-06-22 17:34:03 +0200

    add social media card component to repository

---
+++ social-media-card/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import SocialMediaCard from '@/components/SocialMediaCard.vue'
+</script>
+
+<template>
+  <div class="main-wrapper h-full w-full flex justify-center items-center">
+    <SocialMediaCard />
+  </div>
+</template>
+
+<style scoped>
+.main-wrapper {
+  background: #020024;
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+}
+</style>
